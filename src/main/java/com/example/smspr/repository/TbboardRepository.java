14d911fd55f07e46d74ccfbaf6107b5f1fcee849
jungwonchae <jungwonchae@Jungwons-MacBook-Pro.local>
2024-07-12 22:20:28 +0900

    service, mapper, dto

--- src/main/java/com/example/smspr/repository/TbboardRepository.java
+++ src/main/java/com/example/smspr/repository/TbboardRepository.java
@@ -4,6 +4,8 @@ import com.example.smspr.domain.Tbboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//JpaRepository > 간단하게 DB와 데이터 접근 작업 처리 가능
+//@Entity로 선언한 class가 결국 JPARepository를 사용하여 access할 class
 @Repository
 public interface TbboardRepository extends JpaRepository<Tbboard, String> {
 }
